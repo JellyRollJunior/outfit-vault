412a4e1643b06159c089f2591bc1d385ed24c1cc
Brandon Lin <brandonlin9716@gmail.com>
2021-10-25 22:25:05 -0700

    Optimize imports.

--- app/src/main/java/com/example/outfitvault/MainActivity.java
+++ app/src/main/java/com/example/outfitvault/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.outfitvault;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.outfitvault.model.DataBaseHelper;
 import com.example.outfitvault.model.Outfit;
