200a47d433220240467cf7ffa88c7ebd3a32e2e5
Brandon Lin <brandonlin9716@gmail.com>
2021-10-25 17:59:33 -0700

    Write Outfit.java getters, setters, constructor, toString.

--- app/src/main/java/com/example/outfitvault/model/Outfit.java
+++ app/src/main/java/com/example/outfitvault/model/Outfit.java
@@ -1,5 +1,7 @@
 package com.example.outfitvault.model;
 
+import com.example.outfitvault.types.Season;
+
 public class Outfit {
     private String image;
     private String outer;
@@ -7,5 +9,84 @@ public class Outfit {
     private String bottom;
     private String footwear;
     private String description;
-    
+    private Season season;
+
+    public Outfit(String image, String outer, String inner, String bottom, String footwear, String description, Season season) {
+        this.image = image;
+        this.outer = outer;
+        this.inner = inner;
+        this.bottom = bottom;
+        this.footwear = footwear;
+        this.description = description;
+        this.season = season;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getOuter() {
+        return outer;
+    }
+
+    public void setOuter(String outer) {
+        this.outer = outer;
+    }
+
+    public String getInner() {
+        return inner;
+    }
+
+    public void setInner(String inner) {
+        this.inner = inner;
+    }
+
+    public String getBottom() {
+        return bottom;
+    }
+
+    public void setBottom(String bottom) {
+        this.bottom = bottom;
+    }
+
+    public String getFootwear() {
+        return footwear;
+    }
+
+    public void setFootwear(String footwear) {
+        this.footwear = footwear;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Season getSeason() {
+        return season;
+    }
+
+    public void setSeason(Season season) {
+        this.season = season;
+    }
+
+    @Override
+    public String toString() {
+        return "Outfit{" +
+                "image='" + image + '\'' +
+                ", outer='" + outer + '\'' +
+                ", inner='" + inner + '\'' +
+                ", bottom='" + bottom + '\'' +
+                ", footwear='" + footwear + '\'' +
+                ", description='" + description + '\'' +
+                ", season=" + season +
+                '}';
+    }
 }
