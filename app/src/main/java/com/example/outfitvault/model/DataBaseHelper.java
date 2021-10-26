405c0e737b999f8ab698ae4c1acd35f631366318
Brandon Lin <brandonlin9716@gmail.com>
2021-10-25 19:51:20 -0700

    Create outfit_table schema.

--- app/src/main/java/com/example/outfitvault/model/DataBaseHelper.java
+++ app/src/main/java/com/example/outfitvault/model/DataBaseHelper.java
@@ -8,14 +8,26 @@ import androidx.annotation.Nullable;
 
 public class DataBaseHelper extends SQLiteOpenHelper {
 
+    public static final String OUTFIT_TABLE = "OUTFIT_TABLE";
+    public static final String COLUMN_ID = "ID";
+    public static final String COLUMN_IMAGE_NAME = "IMAGE_NAME";
+    public static final String COLUMN_DESCRIPTION = "DESCRIPTION";
+    public static final String COLUMN_SEASON = "SEASON";
+
     public DataBaseHelper(@Nullable Context context) {
         super(context, "outfit.db", null, 1);
     }
 
-    // called first time db is generated - create table schema here
+    // Create table schema here
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String createTableStatement = "CREATE TABLE " + OUTFIT_TABLE + " (" +
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_IMAGE_NAME + " TEXT, " +
+                COLUMN_DESCRIPTION + " TEXT, " +
+                COLUMN_SEASON + " TEXT)";
 
+        sqLiteDatabase.execSQL(createTableStatement);
     }
 
     // called when version number changes
