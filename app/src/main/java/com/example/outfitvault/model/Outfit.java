3a08c8d7b9944fc469d5aa4c332f03808932d366
Brandon Lin <brandonlin9716@gmail.com>
2021-10-25 17:55:17 -0700

    Create Season and Outfit class.

---
+++ app/src/main/java/com/example/outfitvault/model/Outfit.java
@@ -0,0 +1,11 @@
+package com.example.outfitvault.model;
+
+public class Outfit {
+    private String image;
+    private String outer;
+    private String inner;
+    private String bottom;
+    private String footwear;
+    private String description;
+    
+}
