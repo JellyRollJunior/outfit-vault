5aa50c25f552c291910f97fa752ba0bebd93fc83
Brandon Lin <brandonlin9716@gmail.com>
2021-10-26 21:23:19 -0700

    Refactor OutfitRecViewAdapter.

---
+++ app/src/main/java/com/example/outfitvault/view/OutfitRecViewAdapter.java
@@ -0,0 +1,22 @@
+package com.example.outfitvault.view;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.outfitvault.model.Outfit;
+
+import java.util.ArrayList;
+
+public class OutfitRecViewAdapter {
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
