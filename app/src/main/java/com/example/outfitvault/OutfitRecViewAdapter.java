bdcdf3d0ec9a2f45267576470af826e377331781
Brandon Lin <brandonlin9716@gmail.com>
2021-10-26 22:12:38 -0700

    Reorganize project structure.

--- app/src/main/java/com/example/outfitvault/OutfitRecViewAdapter.java
+++ app/src/main/java/com/example/outfitvault/OutfitRecViewAdapter.java
@@ -1,13 +1,15 @@
-package com.example.outfitvault.view;
+package com.example.outfitvault;
 
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class OutfitRecViewAdapter {
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView outfitImage;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
