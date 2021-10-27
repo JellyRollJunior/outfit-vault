000945997669a8a72cb7855ff4602ba3b217055b
Brandon Lin <brandonlin9716@gmail.com>
2021-10-26 21:40:37 -0700

    Reorganize layouts.

--- app/src/main/java/com/example/outfitvault/view/OutfitRecViewAdapter.java
+++ app/src/main/java/com/example/outfitvault/view/OutfitRecViewAdapter.java
@@ -1,15 +1,9 @@
 package com.example.outfitvault.view;
 
-import android.content.Context;
 import android.view.View;
-import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.example.outfitvault.model.Outfit;
-
-import java.util.ArrayList;
 
 public class OutfitRecViewAdapter {
 
