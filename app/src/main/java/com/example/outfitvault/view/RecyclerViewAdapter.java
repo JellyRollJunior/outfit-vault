5a260c6e0f9197855cdc2a29421816c53cd4a9b1
Brandon Lin <brandonlin9716@gmail.com>
2021-10-26 21:07:03 -0700

    Override RecyclerViewAdapter methods.

--- app/src/main/java/com/example/outfitvault/view/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/outfitvault/view/RecyclerViewAdapter.java
@@ -1,4 +1,42 @@
 package com.example.outfitvault.view;
 
-public class RecyclerViewAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.outfitvault.model.Outfit;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    public RecyclerViewAdapter(Context context, ArrayList<Outfit> outfits) {
+
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
