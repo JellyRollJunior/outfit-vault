54d5e9a4c243bb93d940a42b04d8993137870391
Brandon Lin <brandonlin9716@gmail.com>
2021-10-27 20:29:21 -0700

    Create prototype activity_outfit_view.xml layout.

--- app/src/main/java/com/example/outfitvault/OutfitViewActivity.java
+++ app/src/main/java/com/example/outfitvault/OutfitViewActivity.java
@@ -3,6 +3,8 @@ package com.example.outfitvault;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class OutfitViewActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class OutfitViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_outfit_view);
+
+        Button btn = findViewById(R.id.btnEditOutfit);
+        btn.setVisibility(View.GONE);
     }
 }
