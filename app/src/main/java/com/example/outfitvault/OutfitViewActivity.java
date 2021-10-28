54e9f253751ad5b04687457034adc685eca02c2a
Brandon Lin <brandonlin9716@gmail.com>
2021-10-27 20:31:18 -0700

    Create intent maker in OutfitViewActivity.java.

--- app/src/main/java/com/example/outfitvault/OutfitViewActivity.java
+++ app/src/main/java/com/example/outfitvault/OutfitViewActivity.java
@@ -2,6 +2,8 @@ package com.example.outfitvault;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,5 +17,9 @@ public class OutfitViewActivity extends AppCompatActivity {
 
         Button btn = findViewById(R.id.btnEditOutfit);
         btn.setVisibility(View.GONE);
+    }
+
+    public static Intent makeIntent(Context context) {
+        return new Intent(context, OutfitViewActivity.class);
     }
 }
