15072a8c946e87303276c98e5095977d2749dc3b
Brandon Lin <brandonlin9716@gmail.com>
2021-10-27 21:16:49 -0700

    Refactor code.

--- app/src/main/java/com/example/outfitvault/OutfitViewActivity.java
+++ app/src/main/java/com/example/outfitvault/OutfitViewActivity.java
@@ -1,12 +1,10 @@
 package com.example.outfitvault;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class OutfitViewActivity extends AppCompatActivity {
 
