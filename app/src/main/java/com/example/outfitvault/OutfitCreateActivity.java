4377ccd0d7b51b4d563a848b1b4d8580b48480c8
Brandon Lin <brandonlin9716@gmail.com>
2021-10-30 14:20:26 -0700

    Create OutfitCreateActivity.

---
+++ app/src/main/java/com/example/outfitvault/OutfitCreateActivity.java
@@ -0,0 +1,14 @@
+package com.example.outfitvault;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OutfitCreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_outfit_create);
+    }
+}
