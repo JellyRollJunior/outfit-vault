8f0610372c3bc95707ed2c37d12858b923481277
Brandon Lin <brandonlin9716@gmail.com>
2021-10-30 17:00:43 -0700

    Implement create button functionality.

--- app/src/main/java/com/example/outfitvault/OutfitCreateActivity.java
+++ app/src/main/java/com/example/outfitvault/OutfitCreateActivity.java
@@ -10,32 +10,48 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
-import android.widget.Switch;
 import android.widget.Toast;
 
+import com.example.outfitvault.model.DataBaseHelper;
 import com.example.outfitvault.model.Outfit;
 import com.example.outfitvault.types.Season;
 
-import java.lang.reflect.Array;
-
 public class OutfitCreateActivity extends AppCompatActivity {
+    private boolean isFavorite = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_outfit_create);
 
-        // ID - IMAGENAME - DESCRIPTION - SEASON - FAVORITE
+        // IMAGESAVE + NAME - DESCRIPTION
+        populateSpinner();
+        wireFavoriteButton();
+        wireSetImageButton();
+    }
+
+    private void wireSetImageButton() {
+        // TODO: move to camera -> start activity for result image name
+    }
+
+    private void wireFavoriteButton() {
+        Button favoriteBtn = findViewById(R.id.outfitCreateSetFavoriteButton);
+        favoriteBtn.setOnClickListener(view -> {
+            isFavorite = !isFavorite;
+        });
+    }
+
+    private void populateSpinner() {
         Spinner seasonSpinner = findViewById(R.id.outfitCreateSeasonSpinner);
-        ArrayAdapter spinnerAdapter = new ArrayAdapter(
+        ArrayAdapter<Season> spinnerAdapter = new ArrayAdapter<Season>(
                 OutfitCreateActivity.this,
                 android.R.layout.simple_spinner_dropdown_item,
                 Season.values()
         );
         seasonSpinner.setAdapter(spinnerAdapter);
-
     }
 
     @Override
@@ -49,17 +65,35 @@ public class OutfitCreateActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch(item.getItemId()) {
             case R.id.outfitCreateMenuCreate:
-                Toast.makeText(OutfitCreateActivity.this, "im working guys", Toast.LENGTH_SHORT).show();
-                compileOutfitDetails();
-                finish();
+                Outfit newOutfit = compileOutfitDetails();
+                if (addToDatabase(newOutfit)) {
+                    Toast.makeText(OutfitCreateActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                };
+                // finish();
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
-    private void compileOutfitDetails() {
-        // create and return outfit
-        // Outfit newOutfit = new Outfit()
+    private boolean addToDatabase(Outfit newOutfit) {
+        DataBaseHelper dataBaseHelper = new DataBaseHelper(OutfitCreateActivity.this);
+        return dataBaseHelper.addOne(newOutfit);
+    }
+
+    private Outfit compileOutfitDetails() {
+        // TODO: get image name
+        String imageName = "sample name";
+
+        EditText editTextDescription = findViewById(R.id.outfitCreateDescriptionET);
+        String description = editTextDescription.getText().toString();
+
+        Spinner seasonSpinner = findViewById(R.id.outfitCreateSeasonSpinner);
+        Season season = (Season) seasonSpinner.getSelectedItem();
+
+        Outfit newOutfit = new Outfit(100, imageName, description, season, isFavorite);
+        // debug
+        Toast.makeText(OutfitCreateActivity.this, newOutfit.toString(), Toast.LENGTH_LONG).show();
+        return newOutfit;
     }
 
     public static Intent makeIntent(Context context) {
